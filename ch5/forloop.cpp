d70581f8cc8de5f61958b9079c35b53ca98f1896
jww <jwwbupt@163.com>
2017-06-09 09:22:04 +0800

    learning the C++ Primer Plus chapter Five 2017/06/09

---
+++ ch5/forloop.cpp
@@ -0,0 +1,15 @@
+// C++ Primer Plus Page 125
+// forloop.cpp --- introducing the for loop
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int i;
+	// initialize;test; update
+	for(i = 0; i< 5; i++)
+		cout << "C++ knows loops.\n";
+	cout << "C++ knows when to stop.\n";
+
+	return 0;
+}
